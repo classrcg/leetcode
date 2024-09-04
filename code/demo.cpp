506b93dfeb14faa60b7cb1021b53e02e19d3cea4
茹晨广 <ruchenguang@ruchenguangdeMacBook-Air.local>
2024-09-04 15:58:50 +0800

    git rule

--- code/demo.cpp
+++ code/demo.cpp
@@ -5,6 +5,9 @@ int main()
     for(int i=0; i <10;i++){
         cout<<i<<endl;
     }
+    //git add .
+    //git commit -m "commit message"
+    //git push
     cout<<"1"<<endl;
     cout<<"Hello 11World!"<<endl;
     cout<<"Hello World!"<<endl;
