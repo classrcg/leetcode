e770b8c4e198ecffc6c1cf76a1b116852d1a5b81
茹晨广 <ruchenguang@ruchenguangdeMacBook-Air.local>
2024-09-04 15:34:04 +0800

    init commit

---
+++ code/demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=0; i <10;i++){
+        cout<<i<<endl;
+    }
+    cout<<"Hello 11World!"<<endl;
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
